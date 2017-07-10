0f602da7364457df882829946a3ba2139e6b66ee
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-07-10 12:16:40 +0200

    add mobile menu display, sticky menu bar for tablet and desktop

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(function(){
+    var menuBar = $('#menu-bar');
+    var menuButton = $('#menu-toggle');
+    var menuContent = $('#menu-content');
+    var menuBackground = $('#menu-background');
+
+
+    /*
+    **
+    *** MENU EFFECTS ***
+    **
+    */
+
+
+    /* mobile menu toggle */
+    menuButton.on('click', function(){
+        menuContent.fadeToggle();
+        menuBar.toggleClass('shadow-bar');
+        menuBackground.fadeToggle(1200);
+    });
+
+    /* desktop and tablet sticky menu */
+
+    var tablet = window.matchMedia("screen and (max-width: 1023px) and (min-width: 760px)");
+    var desktop = window.matchMedia("screen and (min-width: 1024px)");
+
+    function useStickyMenu(event){
+        if (event.matches) {
+            var menuBarPosition = menuBar.offset().top;
+            $(window).on('scroll', function(){
+                var scrolledAmount = $(document).scrollTop();
+                if (scrolledAmount > menuBarPosition) {
+                    menuBar.addClass('sticky');
+                } else {
+                    menuBar.removeClass('sticky');
+                }
+            });
+        }
+    }
+
+    useStickyMenu(tablet);
+    useStickyMenu(desktop);
+
+    tablet.addListener(useStickyMenu);
+    desktop.addListener(useStickyMenu);
+
+});
