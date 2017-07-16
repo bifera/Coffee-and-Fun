7f75fef57bb6284010f373e5802dcd5e41640153
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-07-16 10:15:06 +0200

    add jquery-touchswipe plugin

--- coffee-wp/wp-content/themes/anfrawer/footer.php
+++ coffee-wp/wp-content/themes/anfrawer/footer.php
@@ -1,35 +1,36 @@
-        <footer class="scrolled">
-            <section class="container padded" id="contact">
-                <h2>Kontakt</h2>
-                <article>
-                    <div class="columns">
-                        <div>
-                            <p>
-                                Anfrawer Bartosz Bielicki <br />
-                                Leśnych Skrzatów 1 <br />
-                                60-193 Poznań
-                            </p>
-                        </div>
+<footer class="scrolled">
+    <section class="container padded" id="contact">
+        <h2>Kontakt</h2>
+        <article>
+            <div class="columns">
+                <div>
+                    <p>
+                        Anfrawer Bartosz Bielicki <br />
+                        Leśnych Skrzatów 1 <br />
+                        60-193 Poznań
+                    </p>
+                </div>
 
-                        <p>
-                            <span class="fa fa-phone-square"></span> +48 601 422 422 <br />
-                            <span class="fa fa-envelope-square"></span> bartosz@anfrawer.pl <br />
-                            <a href="https://www.facebook.com/anfrawer/" target="_blank"><span class="fa fa-facebook-square"></span> anfrawer</a>
-                        </p>
-                    </div>
-                    <div>
-                    </div>
-                </article>
-
-            </section>
-            <div class="footer-image">
-                <section class="copyright">
-                    <p class="container">Anfrawer - Coffee and Cocoa Trade · Bartosz Bielicki ul. Leśnych Skrzatów 1 · 60-193 Poznań &copy;&nbsp;2016-2017 Anfrawer</p>
-                </section>
+                <p>
+                    <span class="fa fa-phone-square"></span> +48 601 422 422 <br />
+                    <span class="fa fa-envelope-square"></span> bartosz@anfrawer.pl <br />
+                    <a href="https://www.facebook.com/anfrawer/" target="_blank"><span class="fa fa-facebook-square"></span> anfrawer</a>
+                </p>
             </div>
-        </footer>
-        <script src="https://use.fontawesome.com/b416a3a8ab.js"></script>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src="<?php bloginfo('template_url')?>/js/app.js" type="text/javascript"></script>
-    </body>
+            <div>
+            </div>
+        </article>
+
+    </section>
+    <div class="footer-image">
+        <section class="copyright">
+            <p class="container">Anfrawer - Coffee and Cocoa Trade · Bartosz Bielicki ul. Leśnych Skrzatów 1 · 60-193 Poznań &copy;&nbsp;2016-2017 Anfrawer</p>
+        </section>
+    </div>
+</footer>
+<script src="https://use.fontawesome.com/b416a3a8ab.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="<?php bloginfo('template_url')?>/js/jquery.touchSwipe.min.js" type="text/javascript" ></script>
+<script src="<?php bloginfo('template_url')?>/js/app.js" type="text/javascript"></script>
+</body>
 </html>
