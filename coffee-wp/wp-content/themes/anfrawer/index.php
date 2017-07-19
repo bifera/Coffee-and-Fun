80775d0622e77bd626d8903893bc996498f61105
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-07-19 13:01:09 +0200

    add different posts display

--- coffee-wp/wp-content/themes/anfrawer/index.php
+++ coffee-wp/wp-content/themes/anfrawer/index.php
@@ -5,31 +5,28 @@
     </div>
     <section class="container padded scrolled" id="products">
         <h2>Produkty</h2>
-        <div class="slider">
-            <a class="wrapper-arrow" id="previousPost">&lt;</a>
-            <div class="post-wrapper">
-                <div class="wrapper-content">
-                    <?php
-                    $args = ['post_type' => 'kawa'];
-                    $loop = new WP_Query( $args ); 
 
-                    while ( $loop->have_posts() ) : $loop->the_post();
+        <div class="wp-products-box">
+            <h3>Afryka - Kamerun</h3>
+            <div class="products-posts-box">
+                <?php
+                $args = ['post_type' => 'kawa'];
+                $loop = new WP_Query( $args ); 
 
-                    ?>
-                    <article class="post">
-                        <h3><?php the_title();?></h3>
-                        <div class="post-content">
-                            <div class="image-content" style="background-image: url(<?php the_field('wstaw_zdjecie')?>)"></div>
-                            <?php the_content(); ?>
-                        </div>
-                    </article>
-                    <?php
-                    endwhile;
-                    ?>
-                </div>
+                while ( $loop->have_posts() ) : $loop->the_post();
+
+                ?>
+                <article class="products-post">
+                    <div class="products-image-content" style="background-image: url(<?php the_field('wstaw_zdjecie')?>)"></div>
+                    <h4><?php the_title();?></h4>
+                    <div class="products-post-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+                <?php
+                endwhile;
+                ?>
             </div>
-            <a class="wrapper-arrow" id="nextPost">&gt;</a>
-            <div class="post-indicator" id="post-indicator"></div>
         </div>
     </section>
     <section class="container gallery scrolled" id="gallery">
--- coffee-wp/wp-content/themes/anfrawer/js/app.js
+++ coffee-wp/wp-content/themes/anfrawer/js/app.js
@@ -112,7 +112,6 @@ $(function(){
     function smoothScrolling(target){
         var anchor = $(target).attr('href');
         givenOffset = $(anchor).offset().top;
-        console.log(givenOffset);
         $('html, body').animate({scrollTop: givenOffset}, 1200);
     }
 
@@ -125,124 +124,11 @@ $(function(){
         e.preventDefault();
         smoothScrolling($(this));
     });
-    
+
     stickyLogo.on('click', function(e){
         e.preventDefault();
         smoothScrolling($(this));
-    })
-
-
-    /* post slider */
-
-    var nextPostButton = $('#nextPost');
-    var prevPostButton = $('#previousPost');
-    var posts = $('.post');
-    var postsList = $('.wrapper-content');
-    var currentPostIndex = 1;
-
-
-    var postWidth = $('.post').eq(currentPostIndex).width();
-    postsList.width(postWidth*(posts.length+2));
-    postsList.css('position', 'relative');
-    postsList.css('left', postWidth*(-1));
-
-    var bulletsAmount = posts.length;
-    console.log(bulletsAmount);
-
-    /* posts amount indicator */
-
-    var singleBullet = $('<span>').addClass('fa').addClass('fa-circle');
-    var postIndicator = $('#post-indicator');
-    var counter = 0;
-    function createPostBullets(){
-        var clonedBullet = singleBullet.clone(true);
-        clonedBullet.appendTo(postIndicator);
-        counter++;
-        if (counter < bulletsAmount) {
-            createPostBullets();
-        }
-    }
-    createPostBullets();
-
-
-    function postSlider(){
-
-        var firstPostClone = posts.first().clone(true);
-        var lastPostClone = posts.last().clone(true);
-
-        postsList.append(firstPostClone);
-        postsList.prepend(lastPostClone);
-
-        var bullets = $('span.fa-circle'); // displaying posts amount
-        bullets.eq(currentPostIndex-1).addClass('active');
-        bullets.each(function(index, value){
-            $(this).attr('data-number', index+1); 
-        });
-
-        function animateSlider(){
-            bullets.each(function(){
-                $(this).removeClass('active');
-            });
-            bullets.eq(currentPostIndex-1).addClass('active');
-            if (currentPostIndex === posts.length+1) {
-                postsList.animate({left: (currentPostIndex*postWidth)*-1}, 1200, function(){
-                    postsList.css('left', (postWidth*-1));
-                });
-                currentPostIndex = 1;
-                bullets.eq(currentPostIndex-1).addClass('active');
-            } else if (currentPostIndex === 0) {
-                postsList.animate({left: (currentPostIndex*postWidth)*-1}, 1200, function(){
-                    postsList.css('left', (postWidth*(posts.length))*-1);
-                });
-                currentPostIndex = posts.length;
-                bullets.eq(currentPostIndex-1).addClass('active');
-            } else {
-                postsList.animate({left: (currentPostIndex*postWidth)*-1}, 1200);
-                bullets.eq(currentPostIndex-1).addClass('active');
-            }
-        }
-
-        function startTheSlider(event) {
-            currentPostIndex = currentPostIndex+event.data.value;
-            animateSlider();
-        }
-
-        nextPostButton.on('click', {value: +1}, startTheSlider);
-        prevPostButton.on('click', {value: -1}, startTheSlider);
-
-        bullets.each(function(){
-            $(this).on('click', function(){
-                currentPostIndex = $(this).data('number');
-                animateSlider();
-            });
-        });
-
-        // animation on swipe: for mobile
-
-
-        $('.post-wrapper').swipe({
-            swipe:function(event, direction, distance, duration, fingerCount) {
-                if (direction == 'right') {
-                    currentPostIndex--;
-                    animateSlider();
-                } else if (direction == 'left') {
-                    currentPostIndex++;
-                    animateSlider();
-                }
-            }, allowPageScroll:"vertical" // throws warnings, however it works
-        });
-
-        $(window).on('resize', function(){
-            postsList.css('width', "");
-            postWidth = $('.post').eq(1).width();
-            postsList.css('left', postWidth*(-1));
-            postsList.width(postWidth*(posts.length+2));
-            currentPostIndex = 1;
-        });
-
-    }
-
-    postSlider();
+    });
 
     /* scroll delay */
 
@@ -261,6 +147,33 @@ $(function(){
     }
 
     scrollSectionsDelay();
+
+    /*
+    **
+    *** POSTS DISPLAY EFFECTS ***
+    **
+    */
+    var products = $('.products-post');
+    var productsBoxDistance = $('.products-posts-box').offset().left;
+    var productsBoxWidth = $('.products-posts-box').innerWidth();
+    console.log(productsBoxDistance);
+    function displaySinglePostInfo() {
+        products.each(function(){
+            $(this).on('click', function(){
+                var distanceLeft = $(this).offset().left;
+                var popupDisplay = {
+                    width: productsBoxWidth,
+                    position: "absolute",
+                    left: productsBoxDistance-distanceLeft,
+                    top: 0,
+                    "z-index": 2
+                }
+                console.log("distance left: " + distanceLeft);
+                $(this).find('.products-post-content').css(popupDisplay).fadeToggle();
+            });
+        });
+    }
+    displaySinglePostInfo();
 
     /*
     **
