ad224defa5bcc0c73ddd5732d0f01a6ce863cb3c
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-07-24 13:28:52 +0200

    update gallery and fonts subset

--- coffee-wp/wp-content/themes/anfrawer/header.php
+++ coffee-wp/wp-content/themes/anfrawer/header.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php bloginfo('name'); ?></title>
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>">
-        <link href="https://fonts.googleapis.com/css?family=Oswald|PT+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Oswald|PT+Sans&subset=latin,latin-ext" rel="stylesheet">
         <?php wp_head(); ?>
     </head>
     <body>
