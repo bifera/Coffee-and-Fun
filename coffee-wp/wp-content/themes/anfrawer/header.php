7eb8c40a678a3317f35c7ebf0e651e23e1842c75
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-07-24 15:34:38 +0200

    update

--- coffee-wp/wp-content/themes/anfrawer/header.php
+++ coffee-wp/wp-content/themes/anfrawer/header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="pl">
+<html lang="pl-PL">
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
