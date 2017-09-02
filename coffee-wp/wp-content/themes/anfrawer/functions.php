51615f2f6c4a43b801dbdff3587553ce3538758f
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-09-02 10:26:10 +0200

    update functions

--- coffee-wp/wp-content/themes/anfrawer/functions.php
+++ coffee-wp/wp-content/themes/anfrawer/functions.php
@@ -1,5 +1,16 @@
 <?php 
 
+function anfrawer_filter_front_page_template( $template ) {
+    return is_home() ? '' : $template;
+}
+add_filter( 'frontpage_template', 'anfrawer_filter_front_page_template' );
+
+
+function register_my_menu() {
+  register_nav_menu( 'primary', __( 'Primary Menu', 'anfrawer' ) );
+}
+add_action( 'after_setup_theme', 'register_my_menu' );
+
 include 'post-types/kawa.php';
 include 'post-types/miejsce.php';
 include 'post-types/galeria.php';
